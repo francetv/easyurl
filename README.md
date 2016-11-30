7425a4e8416235d6b349ab5127be96c37b5808b5
Nathan Navarro <nathan.navarro.ext@francetv.fr>
2016-11-30 15:55:22 +0100

    CommonJS support + mocha/istanbul for testing and coverage.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This module defines simple tools for parsing or formating URLs.
 
 Installation
 --------------
-This library has been declined in a bower component so in order to use it just add it to your project's bower.json dependencies :
+This library has been declined in a npm module so in order to use it just add it to your project's package.json dependencies :
 
 ```json
 "dependencies": {
@@ -19,23 +19,8 @@ This library has been declined in a bower component so in order to use it just a
 How to use it
 --------------
 
-This library implements [UMD](http://bob.yexley.net/umd-javascript-that-runs-anywhere/), so you can import it with AMD or browser globals
+This library implements [CommonJS]
 
 ```javascript
-require.config({
-    ...
-    paths: {
-        'EasyUrl': './bower_components/EasyUrl/EasyUrl.min.js'
-    }
-})
-require(['EasyUrl', ...], function (EasyUrl, ...) {
-    ...
-});
+const EasyUrl = require('EasyUrl');
 ```
-
-or
-
-```html
-<script type="text/javascript" src="./bower_components/EasyUrl/EasyUrl.min.js" />
-```
-
--- src/EasyUrl.js
+++ src/EasyUrl.js
@@ -1,340 +1,322 @@
-/* global define */
+/**
 
-(function (global) {
-  'use strict';
+Composant d'origine :
+    - href
 
-  function factory () {
-    /**
+Composants brut :
 
-    Composant d'origine :
-        - href
+    - protocol (avec : et en minuscule)
+    - slashedProtocol (:// ou :)
+    - auth (sans @ final)
+    - hostname
+    - port (sans :)
+    - pathname (avec / initial)
+    - search (avec ?)
+    - hash (avec #)
 
-    Composants brut :
+Composants complémentaires :
+    - user
+    - pass
+    - host (hostname + port)
+    - path (pathname + search)
+    - query (search parsé en objet JS)
 
-        - protocol (avec : et en minuscule)
-        - slashedProtocol (:// ou :)
-        - auth (sans @ final)
-        - hostname
-        - port (sans :)
-        - pathname (avec / initial)
-        - search (avec ?)
-        - hash (avec #)
+*/
 
-    Composants complémentaires :
-        - user
-        - pass
-        - host (hostname + port)
-        - path (pathname + search)
-        - query (search parsé en objet JS)
+const properties = {
+  protocol: true,
+  slashedProtocol: true,
+  auth: true,
+  hostname: true,
+  port: true,
+  pathname: true,
+  search: true,
+  hash: true,
 
-    */
+  user: false,
+  pass: false,
+  host: false,
+  path: false,
+  query: false
+};
 
-    var properties = {
-      protocol: true,
-      slashedProtocol: true,
-      auth: true,
-      hostname: true,
-      port: true,
-      pathname: true,
-      search: true,
-      hash: true,
+function EasyUrl () {
+  return this._init.apply(this, arguments);
+}
 
-      user: false,
-      pass: false,
-      host: false,
-      path: false,
-      query: false
-    };
+EasyUrl.prototype = {
+  pattern_href: /^(?:([a-z]{1,6}:)(\/\/)?)?(?:([^\/@]*?)@)?(.*?)(?::([^0-9]+))?(\/[^\?]*?)?(\?[^#]*?)?(#.*)?$/i,
 
-    function EasyUrl () {
-      return this._init.apply(this, arguments);
+  _init: function _init (href, relativeTo) {
+    var key;
+
+    if (relativeTo) {
+      if (!(relativeTo instanceof EasyUrl)) {
+        relativeTo = new EasyUrl(relativeTo);
+      }
+
+      this.base = relativeTo;
     }
 
-    EasyUrl.prototype = {
-      pattern_href: /^(?:([a-z]{1,6}\:)(\/\/)?)?(?:([^\/@]*?)@)?(.*?)(?::([^0-9]+))?(\/[^\?]*?)?(\?[^#]*?)?(#.*)?$/i,
-
-      _init: function _init (href, relativeTo) {
-        var key;
-
-        if (relativeTo) {
-          if (!(relativeTo instanceof EasyUrl)) {
-            relativeTo = new EasyUrl(relativeTo);
-          }
-
-          this.base = relativeTo;
-        }
-
-        if (href) {
-          if (typeof href === 'string') {
-            this.href = href;
-          } else {
-            for (key in href) {
-              if (key in properties) {
-                this[key] = href[key];
-              }
-            }
+    if (href) {
+      if (typeof href === 'string') {
+        this.href = href;
+      } else {
+        for (key in href) {
+          if (key in properties) {
+            this[key] = href[key];
           }
         }
-      },
-
-      get auth () {
-        return EasyUrl.formatAuth({ user: this.user, pass: this.pass });
-      },
-
-      set auth (auth) {
-        delete this.user;
-        delete this.pass;
-
-        if (typeof auth === 'string') {
-          auth = EasyUrl.parseAuth(auth);
-        }
-
-        if (auth.user) {
-          this.user = auth.user;
-          if (auth.pass) {
-            this.pass = auth.pass;
-          }
-        }
-      },
-
-      get host () {
-        return EasyUrl.buildHost(this.hostname, this.port);
-      },
-
-      get path () {
-        return EasyUrl.buildPath(this.pathname, this.query);
-      },
-
-      get search () {
-        return EasyUrl.formatQuery(this.query);
-      },
-
-      set search (search) {
-        this.query = EasyUrl.parseQuery(search);
-      },
-
-      get href () {
-        return this.toString();
-      },
-
-      set href (href) {
-        var parsedUrl = EasyUrl.parse(href, this.base);
-
-        Object.keys(parsedUrl).forEach(function (key) {
-          this[key] = parsedUrl[key];
-        }.bind(this));
-      },
-
-      toObject: function toObject (simple) {
-        var object = {};
-        var key;
-
-        for (key in properties) {
-          if (!simple || properties[key]) {
-            object[key] = this[key];
-          }
-        }
-
-        return object;
-      },
-
-      toString: function toString () {
-        return EasyUrl.format(this);
       }
-    };
+    }
+  },
 
-    EasyUrl.pattern_url = /^(?:([a-z]{1,6}\:)(\/\/)?)?(?:([^\/@]*?)@)?(.*?)(?::([0-9]+))?(\/[^\?]*?)?(\?[^#]*?)?(#.*)?$/i;
+  get auth () {
+    return EasyUrl.formatAuth({ user: this.user, pass: this.pass });
+  },
 
-    EasyUrl.parse = function parse (url, relativeTo) {
-      var urlMatch = EasyUrl.pattern_url.exec(url);
-      var basePath;
-      var auth;
+  set auth (auth) {
+    delete this.user;
+    delete this.pass;
 
-      var parsedUrl = {};
+    if (typeof auth === 'string') {
+      auth = EasyUrl.parseAuth(auth);
+    }
 
-      if (!urlMatch) {
-        throw new Error('EasyUrl Parse Error on URL: ' + url);
-      }
-
-      parsedUrl.protocol = urlMatch[1] || undefined;
-      parsedUrl.slashedProtocol = parsedUrl.protocol && !!urlMatch[2];
-      parsedUrl.hostname = urlMatch[4];
-      parsedUrl.port = +urlMatch[5] || undefined;
-      parsedUrl.pathname = urlMatch[6];
-      parsedUrl.query = EasyUrl.parseQuery(urlMatch[7]);
-      parsedUrl.hash = urlMatch[8] || undefined;
-
-      if (urlMatch[3]) {
-        auth = EasyUrl.parseAuth(urlMatch[3]);
-        parsedUrl.user = auth.user;
-        parsedUrl.pass = auth.pass;
-      }
-
-      // In all those cases, given URL can't be relative. Paring is finished
-      if (!relativeTo || parsedUrl.protocol || auth || parsedUrl.port) {
-        return parsedUrl;
-      }
-
-      // Given URL is relative to some other URL.
-      // so there's no hostname, it is the begining of the pathname
-      // Let start by fixing this
-      parsedUrl.pathname = parsedUrl.hostname + (parsedUrl.pathname || '');
-      if (parsedUrl.pathname[0] !== '/') {
-        parsedUrl.pathname = '/' + parsedUrl.pathname;
-      }
-
-      if (typeof relativeTo === 'string') {
-        relativeTo = EasyUrl.parse(relativeTo);
-      }
-
-      if (relativeTo.pathname) {
-        basePath = relativeTo.pathname.split('/');
-        basePath.length--;
-        parsedUrl.pathname = basePath.join('/') + parsedUrl.pathname;
-      }
-
-      parsedUrl.protocol = relativeTo.protocol;
-      parsedUrl.slashedProtocol = relativeTo.slashedProtocol;
-      parsedUrl.user = relativeTo.user;
-      parsedUrl.pass = relativeTo.pass;
-      parsedUrl.hostname = relativeTo.hostname;
-      parsedUrl.port = relativeTo.port;
-
-      return parsedUrl;
-    };
-
-    EasyUrl.format = function format (urlObject) {
-      var urlString = '';
-
-      if (urlObject.hostname || urlObject.host) {
-        if (urlObject.protocol) {
-          urlString += urlObject.protocol;
-          if (urlObject.slashedProtocol !== false) {
-            urlString += '//';
-          }
-        }
-
-        if (urlObject.user || urlObject.auth) {
-          urlString += (urlObject.auth || EasyUrl.formatAuth({user: urlObject.user, pass: urlObject.pass})) + '@';
-        }
-
-        urlString += urlObject.host || EasyUrl.buildHost(urlObject.hostname, urlObject.port);
-      }
-
-      urlString += urlObject.path || EasyUrl.buildPath(urlObject.pathname, urlObject.query || urlObject.search);
-
-      urlString += urlObject.hash || '';
-
-      return urlString;
-    };
-
-    EasyUrl.parseAuth = function parseAuth (auth) {
-      var parsed = {};
-      var authColonIdx;
-
-      if (auth) {
-        authColonIdx = auth.indexOf(':');
-        if (authColonIdx !== -1) {
-          parsed.user = decodeURIComponent(auth.slice(0, authColonIdx));
-          parsed.pass = decodeURIComponent(auth.slice(authColonIdx + 1));
-        } else {
-          parsed.user = decodeURIComponent(auth);
-        }
-      }
-
-      return parsed;
-    };
-
-    EasyUrl.formatAuth = function formatAuth (auth) {
-      if (!auth.user) {
-        return;
-      }
-      var result = encodeURIComponent(auth.user);
+    if (auth.user) {
+      this.user = auth.user;
       if (auth.pass) {
-        result += ':' + encodeURIComponent(auth.pass);
+        this.pass = auth.pass;
       }
-      return result;
-    };
+    }
+  },
 
-    EasyUrl.buildHost = function buildHost (hostname, port) {
-      var host = hostname;
+  get host () {
+    return EasyUrl.buildHost(this.hostname, this.port);
+  },
 
-      if (port) {
-        host += ':' + port;
+  get path () {
+    return EasyUrl.buildPath(this.pathname, this.query);
+  },
+
+  get search () {
+    return EasyUrl.formatQuery(this.query);
+  },
+
+  set search (search) {
+    this.query = EasyUrl.parseQuery(search);
+  },
+
+  get href () {
+    return this.toString();
+  },
+
+  set href (href) {
+    var parsedUrl = EasyUrl.parse(href, this.base);
+
+    Object.keys(parsedUrl).forEach(function (key) {
+      this[key] = parsedUrl[key];
+    }.bind(this));
+  },
+
+  toObject: function toObject (simple) {
+    var object = {};
+    var key;
+
+    for (key in properties) {
+      if (!simple || properties[key]) {
+        object[key] = this[key];
       }
+    }
 
-      return host;
-    };
+    return object;
+  },
 
-    EasyUrl.buildPath = function buildPath (pathname, search) {
-      var path = pathname || '';
+  toString: function toString () {
+    return EasyUrl.format(this);
+  }
+};
 
-      if (typeof search === 'string') {
-        path += search;
-      } else if (typeof search === 'object') {
-        path += EasyUrl.formatQuery(search);
-      }
+EasyUrl.pattern_url = /^(?:([a-z]{1,6}:)(\/\/)?)?(?:([^\/@]*?)@)?(.*?)(?::([0-9]+))?(\/[^\?]*?)?(\?[^#]*?)?(#.*)?$/i;
 
-      return path;
-    };
+EasyUrl.parse = function parse (url, relativeTo) {
+  var urlMatch = EasyUrl.pattern_url.exec(url);
+  var basePath;
+  var auth;
 
-    EasyUrl.parseQuery = function parseQuery (search) {
-      var query = {};
+  var parsedUrl = {};
 
-      if (!search) {
-        return query;
-      }
-
-      if (search[0] === '?') {
-        search = search.substr(1);
-      }
-
-      search.split('&').forEach(function (param) {
-        var equalIdx;
-
-        if (!param) {
-          return;
-        }
-
-        equalIdx = param.indexOf('=');
-
-        if (equalIdx === -1) {
-          query[decodeURIComponent(param)] = null;
-          return;
-        }
-
-        query[decodeURIComponent(param.substr(0, equalIdx))] = decodeURIComponent(param.substr(equalIdx + 1));
-      });
-
-      return query;
-    };
-
-    EasyUrl.formatQuery = function formatQuery (query) {
-      var search = Object.keys(query).map(function (key) {
-        var param = encodeURIComponent(key);
-        if (query[key] !== null) {
-          param += '=' + encodeURIComponent(query[key]);
-        }
-        return param;
-      }).join('&');
-
-      if (search) {
-        return '?' + search;
-      }
-
-      return '';
-    };
-
-    return EasyUrl;
+  if (!urlMatch) {
+    throw new Error('EasyUrl Parse Error on URL: ' + url);
   }
 
-  if (typeof module === 'object' && module.exports) {
-    module.exports = factory();
-  } else if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define('EasyUrl', [], factory);
-  } else {
-    // Browser globals
-    global.EasyUrl = factory();
+  parsedUrl.protocol = urlMatch[1] || undefined;
+  parsedUrl.slashedProtocol = parsedUrl.protocol && !!urlMatch[2];
+  parsedUrl.hostname = urlMatch[4];
+  parsedUrl.port = +urlMatch[5] || undefined;
+  parsedUrl.pathname = urlMatch[6];
+  parsedUrl.query = EasyUrl.parseQuery(urlMatch[7]);
+  parsedUrl.hash = urlMatch[8] || undefined;
+
+  if (urlMatch[3]) {
+    auth = EasyUrl.parseAuth(urlMatch[3]);
+    parsedUrl.user = auth.user;
+    parsedUrl.pass = auth.pass;
   }
-}(this));
+
+  // In all those cases, given URL can't be relative. Paring is finished
+  if (!relativeTo || parsedUrl.protocol || auth || parsedUrl.port) {
+    return parsedUrl;
+  }
+
+  // Given URL is relative to some other URL.
+  // so there's no hostname, it is the begining of the pathname
+  // Let start by fixing this
+  parsedUrl.pathname = parsedUrl.hostname + (parsedUrl.pathname || '');
+  if (parsedUrl.pathname[0] !== '/') {
+    parsedUrl.pathname = '/' + parsedUrl.pathname;
+  }
+
+  if (typeof relativeTo === 'string') {
+    relativeTo = EasyUrl.parse(relativeTo);
+  }
+
+  if (relativeTo.pathname) {
+    basePath = relativeTo.pathname.split('/');
+    basePath.length--;
+    parsedUrl.pathname = basePath.join('/') + parsedUrl.pathname;
+  }
+
+  parsedUrl.protocol = relativeTo.protocol;
+  parsedUrl.slashedProtocol = relativeTo.slashedProtocol;
+  parsedUrl.user = relativeTo.user;
+  parsedUrl.pass = relativeTo.pass;
+  parsedUrl.hostname = relativeTo.hostname;
+  parsedUrl.port = relativeTo.port;
+
+  return parsedUrl;
+};
+
+EasyUrl.format = function format (urlObject) {
+  var urlString = '';
+
+  if (urlObject.hostname || urlObject.host) {
+    if (urlObject.protocol) {
+      urlString += urlObject.protocol;
+      if (urlObject.slashedProtocol !== false) {
+        urlString += '//';
+      }
+    }
+
+    if (urlObject.user || urlObject.auth) {
+      urlString += (urlObject.auth || EasyUrl.formatAuth({user: urlObject.user, pass: urlObject.pass})) + '@';
+    }
+
+    urlString += urlObject.host || EasyUrl.buildHost(urlObject.hostname, urlObject.port);
+  }
+
+  urlString += urlObject.path || EasyUrl.buildPath(urlObject.pathname, urlObject.query || urlObject.search);
+
+  urlString += urlObject.hash || '';
+
+  return urlString;
+};
+
+EasyUrl.parseAuth = function parseAuth (auth) {
+  var parsed = {};
+  var authColonIdx;
+
+  if (auth) {
+    authColonIdx = auth.indexOf(':');
+    if (authColonIdx !== -1) {
+      parsed.user = decodeURIComponent(auth.slice(0, authColonIdx));
+      parsed.pass = decodeURIComponent(auth.slice(authColonIdx + 1));
+    } else {
+      parsed.user = decodeURIComponent(auth);
+    }
+  }
+
+  return parsed;
+};
+
+EasyUrl.formatAuth = function formatAuth (auth) {
+  if (!auth.user) {
+    return;
+  }
+  var result = encodeURIComponent(auth.user);
+  if (auth.pass) {
+    result += ':' + encodeURIComponent(auth.pass);
+  }
+  return result;
+};
+
+EasyUrl.buildHost = function buildHost (hostname, port) {
+  var host = hostname;
+
+  if (port) {
+    host += ':' + port;
+  }
+
+  return host;
+};
+
+EasyUrl.buildPath = function buildPath (pathname, search) {
+  var path = pathname || '';
+
+  if (typeof search === 'string') {
+    path += search;
+  } else if (typeof search === 'object') {
+    path += EasyUrl.formatQuery(search);
+  }
+
+  return path;
+};
+
+EasyUrl.parseQuery = function parseQuery (search) {
+  var query = {};
+
+  if (!search) {
+    return query;
+  }
+
+  if (search[0] === '?') {
+    search = search.substr(1);
+  }
+
+  search.split('&').forEach(function (param) {
+    var equalIdx;
+
+    if (!param) {
+      return;
+    }
+
+    equalIdx = param.indexOf('=');
+
+    if (equalIdx === -1) {
+      query[decodeURIComponent(param)] = null;
+      return;
+    }
+
+    query[decodeURIComponent(param.substr(0, equalIdx))] = decodeURIComponent(param.substr(equalIdx + 1));
+  });
+
+  return query;
+};
+
+EasyUrl.formatQuery = function formatQuery (query) {
+  var search = Object.keys(query).map(function (key) {
+    var param = encodeURIComponent(key);
+    if (query[key] !== null) {
+      param += '=' + encodeURIComponent(query[key]);
+    }
+    return param;
+  }).join('&');
+
+  if (search) {
+    return '?' + search;
+  }
+
+  return '';
+};
+
+module.exports = EasyUrl;
